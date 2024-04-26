bb2d6f6804d87c2dc8e898c4b25a6077c77df02d
Marli_Vinic <marlissonvinicius13@gmail.com>
2024-04-26 17:35:44 -0300

    2° commit e teste

--- index.java
+++ index.java
@@ -1,9 +1,8 @@
-/**
- * index
- */
+
 public class index {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Deu certo");
     }
 }
